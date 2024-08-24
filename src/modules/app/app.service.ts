60b3d39c8d66666f402571e6d74910a21b46ecb6
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2024-08-24 02:49:18 -0300

    feat: add health bob

--- src/modules/app/app.service.ts
+++ src/modules/app/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async healthBob() {
+    const response = await this.health();
+    return response;
+  }
+
+  async health() {
+    try {
+      const response = await axios.get(`https://bob-os70.onrender.com`);
+
+      return response.data;
+    } catch (error) {
+      console.log('Error: ', error);
+      throw new Error(error.message);
+    }
   }
 }
